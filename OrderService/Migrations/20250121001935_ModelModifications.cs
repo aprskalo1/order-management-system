546adb67c404de062f9d37043df89036bf91c030
aprskalo1 <ante.prskalo4@gmail.com>
2025-01-21 01:54:45 +0100

    Business logic should be complete

---
+++ OrderService/Migrations/20250121001935_ModelModifications.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderService.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelModifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EffectiveDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EffectiveDate",
+                table: "Orders");
+        }
+    }
+}
